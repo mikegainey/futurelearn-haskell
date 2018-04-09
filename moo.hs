a31267a593e370669e66b6d03483dfe4684138c1
Michael Gainey <mikeg9b@gmail.com>
2018-04-09 13:14:57 -0500

    improved the cattle function
    - from recursiona (Ver1)
    - to map and filter (Ver2)
    - to a list comprehension (Ver3)

--- moo.hs
+++ moo.hs
@@ -9,7 +9,7 @@ moo :: Int -> IO ()
 moo secretNum = mooWithCounter secretNum 1
 
 mooWithCounter :: Int -> Int -> IO ()
--- given the secret number and starting count, this is the game loop
+-- given the secret number and starting count; this is the game loop
 mooWithCounter secretNum counter = do
   putStr "\nWhat is your guess? "
   guessStr <- getLine
@@ -17,20 +17,14 @@ mooWithCounter secretNum counter = do
   let (bulls, cows) = pureMoo secretNum guessNum
   if bulls == 4
     then putStrLn $ "You won in " ++ (show counter) ++ " moves!\n"
-    else do putStrLn $ show bulls ++ " bulls, and " ++ show cows ++ " cows"
+    else do putStrLn $ (show bulls) ++ " bulls, and " ++ (show cows) ++ " cows"
             mooWithCounter secretNum (counter + 1)
 
 pureMoo :: Int -> Int -> (Int, Int)
--- given the secret number and the guess, returns a tuple: (bulls, cows)
+-- given the secret number and the guess, return a tuple: (bulls, cows)
 pureMoo secretNum guessNum = (bulls secret guess, cows secret guess)
   where secret = num2list secretNum
         guess  = num2list guessNum
-
-cattle :: [Int] -> [Int] -> Int
--- cattle = the number of all matches (cows + bulls)
-cattle [] guess = 0
-cattle secret guess = length (filter (== head secret) guess)
-                      + cattle (tail secret) guess
 
 bulls :: [Int] -> [Int] -> Int
 -- the number of matches in the correct position
@@ -38,11 +32,25 @@ bulls secret guess = length $ [(s,g) | (s,g) <- zip secret guess, s == g]
 
 cows :: [Int] -> [Int] -> Int
 -- the number of cows = cattle - bulls
-cows secret guess = (cattle secret guess) - (bulls secret guess)
+cows secret guess = (cattleVer3 secret guess) - (bulls secret guess)
+
+cattleVer1 :: [Int] -> [Int] -> Int
+-- cattleVer1 = the number of all matches (cows + bulls)
+cattleVer1 [] guess = 0
+cattleVer1 secret guess = length (filter (== head secret) guess)
+                      + cattleVer1 (tail secret) guess
+
+cattleVer2 :: [Int] -> [Int] -> Int
+-- cattleVer2 = the number of all matches (cows + bulls)
+cattleVer2 secret guess = sum $ map (\s -> length $ filter (==s) guess) secret
+
+cattleVer3 :: [Int] -> [Int] -> Int
+-- cattleVer3 = the number of all matches (cows + bulls)
+cattleVer3 secret guess = length $ [ 1 | s <- secret, g <- guess, s == g]
 
 num2list :: Int -> [Int]
--- takes a multi-digit number and returns the corresponding list of digits
--- example: 1234 -> [1,2,3,4]
+-- take a multi-digit number and return the corresponding list of digits
+-- example: 1234 --> [1,2,3,4]
 num2list n
   | n < 10 = [n]
   | otherwise = num2list (n `div` 10) ++ [(n `mod` 10)]
