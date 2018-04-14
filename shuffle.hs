9170becda58df0775dbe12bca9e5d5cc52521616
Michael Gainey <mikeg9b@gmail.com>
2018-04-14 13:59:39 -0500

    It still doesn't work.

--- shuffle.hs
+++ shuffle.hs
@@ -7,14 +7,9 @@
 
 import System.Random
 
-shuffle1 :: [Int] -> [Int]
-shuffle1 xs = do
+shuffle :: [Int] -> [Int]
+shuffle xs = do
   index <- getStdRandom (randomR (0 :: Int, (length xs) - 1))
   if length xs == 1
     then return xs
-    else return $ xs!!index : shuffle1 (tail xs)
-
-shuffle2 xs
-  | length xs == 1 = xs
-  | otherwise = xs!!index : shuffle2 (tail xs)
-  where do index <- getStdRandom (randomR (0, (length xs) - 1))
+    else return $ xs!!index : shuffle (tail xs)
