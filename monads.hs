73313c04ce0bca1d415dbcc2e0d08f75aa9e3c7c
Michael Gainey <mikeg9b@gmail.com>
2018-04-24 19:55:14 -0500

    defining Maybe in the Monad type class

--- monads.hs
+++ monads.hs
@@ -1,7 +1,23 @@
 -- Try to understand monads
 
-ioint = do
-  putStr "Enter a number: "
-  numstr <- getLine
-  let numnum = read numstr :: Int
-  return numnum
+import Control.Monad
+
+-- instance Monad Maybe where
+--   return         = Just
+--   Nothing >>= f  = Nothing
+--   (Just x) >>= f = f x
+--   fail _         = Nothing
+
+-- instance MonadPlus Maybe where
+--   mzero             = Nothing
+--   Nothing `mplus` x = x
+--   x `mplus` _       = x
+
+myHead :: [a] -> Maybe a
+myHead [] = Nothing
+myHead xs = Just (head xs)
+
+myTail :: [a] -> Maybe [a]
+myTail [] = Nothing
+myTail xs = Just (tail xs)
+
